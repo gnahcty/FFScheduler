934aee27667b19331c96ce63a72b868a5ac375b1
gnahcty <gnahcty@gmail.com>
2024-06-19 17:26:26 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { PrimeVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FFScheduler/',
   plugins: [
     vue({
       template: {
