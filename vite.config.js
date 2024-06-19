ad0edac9df18ed4c1754a0e8f7276863ee8b9d75
gnahcty <gnahcty@gmail.com>
2024-06-19 17:35:58 +0800

    change base

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { PrimeVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/FFScheduler/',
   plugins: [
     vue({
       template: {
